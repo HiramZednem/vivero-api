ea30ee1f701b2adc3555012844da97a15ec958d0
HiramZednem <hirammendez000@gmail.com>
2024-09-07 19:07:46 -0600

    FIX: you can't errase a product if it have a order

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { productService } from '../services';
+import { orderService, productService } from '../services';
 
 
 export const productController = {
@@ -48,8 +48,18 @@ export const productController = {
             if (!productExist) {
                 return res.status(404).json({ error: 'Product not found' });
             }
-            const product = await productService.delete(parseInt(req.params.id));
-            return res.status(200).json(product);
+
+            orderService.getAll().then((orders) => {
+                const ordersWithProduct = orders.filter((order) => order.product_id === parseInt(req.params.id));
+                if (ordersWithProduct.length > 0) {
+                    return res.status(400).json({ error: 'Product is in use' });
+                } else {
+                    productService.delete(parseInt(req.params.id)).then((product) => {
+                        return res.status(200).json(product);
+                    });
+                }
+            });
+            
         } catch (e) {
             res.status(500).json({ error: (e as Error).message });
         }
